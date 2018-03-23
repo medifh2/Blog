2587212a73bf92cd5c65e1a861f2b28a20b659d3
Mikhail Galtsov <medifh3@gmail.com>
2018-03-23 17:27:05 +0700

    1

--- registration/regmodel.php
+++ registration/regmodel.php
@@ -1,5 +1,17 @@
 <?php
-    $connect = mysqli_connect("localhost", "root", "root", "blog");
-    if (!$connect) echo "error";
-    $query =mysqli_query($connect,"INSERT INTO `Users` (`Login`, `Password`, `Username`,`About_me`,`accesslvl`) 
-	                                     VALUES ($login, $pass, $uname, $about, $lvl)");
+class modeldb
+{
+    private $db;
+
+    public
+    function __construct() {
+        $this -> db = mysqli_connect("localhost", "root", "root", "blog");
+    }
+    function add_user($login, $pass, $uname, $about, $lvl)
+    {
+
+        if (!($this -> db)) echo "error";
+        $query = mysqli_query($this -> db, "INSERT INTO `Users` (`Login`, `Password`, `Username`,`About_me`,`accesslvl`)
+                                     VALUES ($login, $pass, $uname, $about, $lvl)");
+    }
+}
