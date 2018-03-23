174a81817b166c8bb2f388581b09e46f31ce5465
Mikhail Galtsov <medifh3@gmail.com>
2018-03-23 19:41:24 +0700

    registration with error in regmodel

--- registration/regcontrol.php
+++ registration/regcontrol.php
@@ -1,37 +1,27 @@
 <?php
-    $host = '127.0.0.1';
-    $db   = 'test';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8';
-
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $opt = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    $pdo = new PDO($dsn, $user, $pass, $opt);
-
     include_once "regmodel.php";
-	$connect = mysqli_connect("localhost", "root", "root", "blog");
-	if (!$connect) echo "error";
+    $db = 'blog';
+    $host = '127.0.0.1';
+    $user = 'root';
+    $pass = 'root';
+    $charset = 'utf8';
+	$connect = new modeldb($db, $host, $user, $pass, $charset);
 
 	{
-		$pass = $_POST["password"];
-		$r_pass = $_POST["r_password"];
+		$pass = $_POST["pass"];
+		$r_pass = $_POST["r_pass"];
 		if ($pass !== $r_pass) die("Passwords do not match!");
-		$login = $_POST["login"];
-		$uname = $_POST["username"];	
-		$about = $_POST["about_me"];
 		$pass = md5($pass);
-		$lvl = "reader";
-		// Use PDO!
-        $query =mysqli_query($connect,"INSERT INTO `Users` (`Login`, `Password`, `Username`,`About_me`,`accesslvl`) 
-		VALUES ($login, $pass, $uname, $about, $lvl)")
-		or die (mysqli_error($connect));
-        echo ("HELLO".$uname);
-
+		$user =
+        [
+            'login' => $_POST["login"],
+            'pass' =>$pass,
+            'name' => $_POST["username"],
+            'about' => $_POST["about_me"],
+            'lvl' => 'reader'
+        ];
+		$connect ->add_user($user);
+        echo ("HELLO".$user['name']);
 	}
 	if (isset($_POST["reg"]))
 	($connect);
--- registration/regmodel.php
+++ registration/regmodel.php
@@ -1,17 +1,39 @@
 <?php
 class modeldb
 {
-    private $db;
+    private $dsn, $opt, $pdo;
 
     public
-    function __construct() {
-        $this -> db = mysqli_connect("localhost", "root", "root", "blog");
+    function __construct($db, $host, $user, $pass, $charset)
+    {
+        $this -> dsn = "mysql:host = $host; db = $db; charset = $charset";
+        $this -> opt =
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $this -> pdo = new PDO($this -> dsn, $user, $pass, $this -> opt);
     }
-    function add_user($login, $pass, $uname, $about, $lvl)
+    function add_user($user)
     {
 
-        if (!($this -> db)) echo "error";
-        $query = mysqli_query($this -> db, "INSERT INTO `Users` (`Login`, `Password`, `Username`,`About_me`,`accesslvl`)
-                                     VALUES ($login, $pass, $uname, $about, $lvl)");
+        if (!($this -> pdo)) echo "error";
+        $st = $this -> pdo -> prepare ('INSERT INTO ? VALUES (?, ?, ?, ?, ?)');
+        $st -> execute(array('Users', $user));
+        //$st = "INSERT INTO ? SET ?";
+        //$st = $this -> pdo -> prepare($st);
+        //$st->execute('users', $user);
+        //$st = "INSERT INTO Users (Login, Password, Username, About_me, accesslvl)
+        //VALUES (:login, :pass, :uname, :about, :lvl)";
+        //$query = $this -> pdo ->prepare($st);
+        //$query->execute( array
+        //(
+        //    ':login' => $user['login'],
+        //    ':pass' => $user['pass'],
+        //    ':uname' => $user['name'],
+        //    ':about' => $user['about'],
+        //    ':lvl' => $user['lvl']
+        //));
     }
 }
